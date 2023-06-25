4a25dc5f91aee6dbd36f25bacb9c6b5d0b1a1a29
yabbing <sabbatino99@gmail.com>
2023-06-25 15:03:27 -0400

    start

---
+++ sabbatino99/thestart/Blackjack.py
@@ -0,0 +1,32 @@
+import random
+
+cards = []
+suits = ['spades', 'clubs', 'hearts', 'diamonds']
+ranks = [
+        {'rank': 'A', 'value': 11},
+        {'rank': '2', 'value': 2},
+        {'rank': '3', 'value': 3},
+        {'rank': '4', 'value': 4},
+        {'rank': '5', 'value': 5},
+        {'rank': '6', 'value': 6},
+        {'rank': '7', 'value': 7},
+        {'rank': '8', 'value': 8},
+        {'rank': '9', 'value': 9},
+        {'rank': '10', 'value': 10}, 
+    ]
+for suit in suits:
+    for rank in ranks:
+        cards.append([suit, rank])
+def shuffle():
+    random.shuffle(cards)
+
+def deal(number):
+    cards_delt = []
+    for x in range(number):
+        card = cards.pop()
+        cards_delt.append(card)
+    return cards_delt
+
+shuffle()
+card = deal(1)[0]
+print(card)

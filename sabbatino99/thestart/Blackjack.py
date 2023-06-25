9219fc058e4a23230841d76c617981466baab550
yabbing <sabbatino99@gmail.com>
2023-06-25 16:58:43 -0400

    stuck

--- sabbatino99/thestart/Blackjack.py
+++ sabbatino99/thestart/Blackjack.py
@@ -1,32 +1,40 @@
 import random
 
-cards = []
-suits = ['spades', 'clubs', 'hearts', 'diamonds']
-ranks = [
-        {'rank': 'A', 'value': 11},
-        {'rank': '2', 'value': 2},
-        {'rank': '3', 'value': 3},
-        {'rank': '4', 'value': 4},
-        {'rank': '5', 'value': 5},
-        {'rank': '6', 'value': 6},
-        {'rank': '7', 'value': 7},
-        {'rank': '8', 'value': 8},
-        {'rank': '9', 'value': 9},
-        {'rank': '10', 'value': 10}, 
-    ]
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
-def shuffle():
-    random.shuffle(cards)
+class Deck:
+    def __int__(self):
+        self.cards = []
+        suits = ['spades', 'clubs', 'hearts', 'diamonds']
+        ranks = [
+                {'rank': 'A', 'value': 11},
+                {'rank': '2', 'value': 2},
+                {'rank': '3', 'value': 3},
+                {'rank': '4', 'value': 4},
+                {'rank': '5', 'value': 5},
+                {'rank': '6', 'value': 6},
+                {'rank': '7', 'value': 7},
+                {'rank': '8', 'value': 8},
+                {'rank': '9', 'value': 9},
+                {'rank': '10', 'value': 10},
+                {'rank': 'J', 'value': 10},
+                {'rank': 'Q', 'value': 10},
+                {'rank': 'K', 'value': 10}, 
+            ]
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
 
-def deal(number):
-    cards_delt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_delt.append(card)
-    return cards_delt
+    def shuffle(self):
+        random.shuffle(self.cards)
 
-shuffle()
-card = deal(1)[0]
-print(card[1]['value'])
+    def deal(self, number):
+        cards_delt = []
+        for x in range(number):
+            card = self.cards.pop()
+            cards_delt.append(card)
+        return cards_delt
+
+deck1 = Deck()
+deck2 = Deck()
+deck2.shuffle()
+print(deck1.cards)
+print(deck2.cards)

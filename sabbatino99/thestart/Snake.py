a8623df5fb5dddf50f31243656c3488a96496e21
yabbing <sabbatino99@gmail.com>
2023-07-07 23:58:29 -0400

    start

---
+++ sabbatino99/thestart/Snake.py
@@ -0,0 +1,51 @@
+import pygame
+pygame.init()
+
+white=(255,255,255)
+black=(0,0,0)
+red=(255,0,0)
+
+
+dis_width = 800
+dis_height = 600
+dis=pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake game by Sab')
+
+game_over= False
+
+x1 = 300
+y1 = 300
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
+while not game_over:
+    for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           game_over = True
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            x1_change = -10
+            y1_change = 0
+        elif event.key == pygame.K_RIGHT:
+            x1_change = 10
+            y1_change = 0
+        elif event.key == pygame.K_UP:
+            x1_change = 0
+            y1_change = -10
+        elif event.key == pygame.K_DOWN:
+            x1_change = 0
+            y1_change = 10
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(white)
+    pygame.draw.rect(dis,black,[x1, y1, 10, 10])
+
+    pygame.display.update()
+
+    clock.tick(30)
+
+pygame.quit()
+quit()
